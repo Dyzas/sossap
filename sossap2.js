56aa060e4e3befc7ccf9d47e388430f696c381f1
maxime Lefebvre <128160700+Dyzas@users.noreply.github.com>
2023-05-18 11:23:59 +0200

    Update sossap2.js

--- sossap2.js
+++ sossap2.js
@@ -1,21 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var modal2 = document.getElementById("myModal2");
-    var btn2 = document.getElementById("myBtn2");
-    var span2 = document.getElementsByClassName("close2")[0];
+// Attend que le document HTML soit complètement chargé
+document.addEventListener('DOMContentLoaded', function() {
+  // Récupère la référence du bouton de la modal
+  var modalBtn = document.getElementById('myBtn');
   
-    btn2.addEventListener("click", function() {
-      modal2.style.display = "block";
-    });
+  // Récupère la référence du bouton de fermeture de la modal
+  var closeBtn = document.querySelector('.modal .close');
   
-    span2.addEventListener("click", function() {
-      modal2.style.display = "none";
-    });
+  // Récupère la référence de la modal
+  var modal = document.getElementById('myModal');
   
-    window.addEventListener("click", function(event) {
-      if (event.target === modal2) {
-        modal2.style.display = "none";
-      }
-    });
+  // Ajoute un gestionnaire d'événement au clic sur le bouton
+  modalBtn.addEventListener('click', function() {
+    // Ajoute la classe "show" à la modal pour l'afficher
+    modal.classList.add('show');
   });
   
-  
+  // Ajoute un gestionnaire d'événement au clic sur le bouton de fermeture
+  closeBtn.addEventListener('click', function() {
+    // Retire la classe "show" de la modal pour la masquer
+    modal.classList.remove('show');
+  });
+});
